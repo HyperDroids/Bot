dce564d1491b9a246a78a0ed69a661cbb9f81f89
HyperDroids <matthewkelly450@hotmail.com>
2018-08-29 02:37:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,47 +11,5 @@ client.on('message', message => {
   	}
 });
 
-
-client.on('message', message => {
-    if (message.content === 'Is Hydroid Zach?') {
-    	message.reply('No, @HyperDroid#2164 is the manager here. Zach is @Zach#4237');
-  	}
-});
-
-
-client.on('message', message => {
-    if (message.content === 'Who made this bot?') {
-    	message.reply('@HyperDroid#2164, of course!');
-  	}
-});
-
-//long comands 
-mybot.on("message", function(message){
-
-            if( message.content === "!my_avatar" ){
-
-        var usersAvatar = message.sender.avatarURL;
-
-        if(usersAvatar){
-            // user has an avatar
-
-            mybot.reply(message, "your avatar can be found at " + usersAvatar);
-
-        }else{
-            // user doesn't have an avatar
-
-            mybot.reply(message, "you don't have an avatar!");
-        }
-
-            }
-
-} );
-
-
-
-
-
-
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
